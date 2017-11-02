52d1d0360456eefbfa86ccc5cdcb08935108e1c2
Kshitij <kjain1995@gmail.com>
2017-11-02 17:20:00 -0400

    First commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls:['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  showModal(){
+    $('.large.modal').transition('slide right').modal('show');
+  }
+
+}
